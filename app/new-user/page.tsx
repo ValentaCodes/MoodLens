f547fad2617ebb7284826dc72745a499dd32cd68
ValentaCodes <valentacodes@gmail.com>
2023-09-18 16:53:12 -0600

    new-user functionality

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,5 +1,34 @@
-const NewUserPage = () => {
-  return <div>Hi</div>
+import { prisma } from '@/utils/db'
+import { auth, currentUser } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
+
+const createNewUser = async () => {
+  try {
+    const user = await currentUser()
+
+    const match = await prisma.user.findUnique({
+      where: {
+        clerkId: user?.id as string,
+      },
+    })
+
+    if (!match) {
+      await prisma.user.create({
+        data: {
+          clerkId: user?.id as string,
+          email: user?.emailAddresses[0].emailAddress as string,
+        },
+      })
+    }
+  } catch (error) {
+    console.error(error)
+  }
+  redirect('/journal')
+}
+
+const NewUserPage = async () => {
+  await createNewUser()
+  return <div>...loading</div>
 }
 
 export default NewUserPage
