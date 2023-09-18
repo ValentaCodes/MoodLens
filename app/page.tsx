eca87166a6fafa00a6a301959fde8e36916cebc1
ValentaCodes <valentacodes@gmail.com>
2023-09-18 16:53:53 -0600

    dynamic href

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
 import Link from 'next/link'
+import { auth } from '@clerk/nextjs'
 
-export default function Home() {
+export default async function Home() {
+
+  // make href dynamic based on user status
+  const { userId } = await auth()
+  let href = userId ? '/journal' : '/new-user'
+
   // In tailwind w-screen&h-screen represent 100%vh/vw
   return (
     <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
@@ -12,7 +18,7 @@ export default function Home() {
           insights into your emotions and experiences.
         </p>
         <div>
-          <Link href={'/journal'}>
+          <Link href={href}>
             <button className="px-4 py-2 rounded-lg text-xl bg-red-700">
               Get Started
             </button>
