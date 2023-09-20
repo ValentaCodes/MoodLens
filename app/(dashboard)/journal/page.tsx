29448b931f40b20014a24eb258bf254a9d5cdde8
ValentaCodes <valentacodes@gmail.com>
2023-09-20 00:57:01 -0600

    fixed some things

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -2,27 +2,49 @@
 // Which means we can fetch all the journal entries right here. no code will make it to the browser.
 import { prisma } from '@/utils/db'
 import { getUserByClerkId } from '@/utils/auth'
-
+import NewEntryCard from '@/components/NewEntryCard'
+import EntryCard from '@/components/EntryCard'
 
 const getEntries = async () => {
-  const user = await getUserByClerkId()
-  console.log('user_data_from_journal', user)
+  try {
+    const user = await getUserByClerkId()
+    console.log('user_data_from_function', user)
 
-  //   try {
-  //     const data = await prisma.journalEntry.findMany({
-  //       where: {
-  //         userId: user as string
-  //       }
-  //     })
-  //     console.log('journal_entries',data);
-  //   } catch (error) {
-  //     console.log(error)
-  //   }
+    // Find journal entries based on clerkId from OUR database
+    const journal_entries = await prisma.journalEntry.findMany({
+      where: {
+        userId: user?.id as string | undefined,
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+    })
+
+    // logging journal entries
+    console.log('journal_entries', journal_entries)
+    return journal_entries
+  } catch (error) {
+    console.log(
+      "Something went wrong, we couldn't locate any journal entries",
+      error
+    )
+  }
 }
 
 const JournalPage = async () => {
-  await getEntries()
-  return <div>{}</div>
+  const entries = await getEntries()
+
+  return (
+    <div className="p-10">
+      <div className="text-2xl mb-8">Journal</div>
+      <div className="grid grid-cols-3 gap-4">
+        <NewEntryCard />
+        {entries?.map((entry) => (
+          <EntryCard key={entry.id} entry={entry} />
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default JournalPage
