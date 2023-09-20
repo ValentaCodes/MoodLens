d7d7d53c6e4569588753a392c75089b2fa515ddb
ValentaCodes <valentacodes@gmail.com>
2023-09-20 00:56:02 -0600

    started work on entry card components

---
+++ components/EntryCard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EntryCard = ({entry}) => {
+  return (
+    <div>{entry}</div>
+  )
+}
+
+export default EntryCard
---
+++ components/NewEntryCard.tsx
@@ -0,0 +1,10 @@
+const NewEntryCard = () => {
+  return (
+    <div className="flex flex-col border h-60 ">
+    <div className="text-1xl ">newEntryCard</div>
+    <input className="placeholder:italic" placeholder="Feel free to start typing..."></input>
+    </div>
+  )
+}
+
+export default NewEntryCard
