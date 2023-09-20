4def6b1e7be43b9655e51061bc12688a61402cdd
ValentaCodes <valentacodes@gmail.com>
2023-09-19 18:14:47 -0600

    dashboard layout and journal data

---
+++ app/(dashboard)/journal/page.tsx
@@ -0,0 +1,26 @@
+// this will be a react server component
+// Which means we can fetch all the journal entries right here. no code will make it to the browser. 
+import { prisma } from '@/utils/db'
+import { User } from '@prisma/client'
+import { currentUser } from '@clerk/nextjs'
+
+const getEntries = async () => {
+  const user = await currentUser()
+  try {
+    const data = await prisma.journalEntry.findMany({
+      where: {
+        userId: user?.id as string
+      }
+    })
+    console.log('journal_entries',data);
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const JournalPage = async () => {
+  await getEntries()
+  return <div>{}</div>
+}
+
+export default JournalPage
