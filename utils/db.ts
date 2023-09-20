dc17ff3ae511808c945a10f11dbf3799f3d8fca1
ValentaCodes <valentacodes@gmail.com>
2023-09-20 00:53:52 -0600

    did somethinf

--- utils/db.ts
+++ utils/db.ts
@@ -1,15 +1,15 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
 // This is how we will make a new instance for prisma client
 // With hot module reloading, multiple database connection objects can be created
 // Causing issues in the application. These utility methods check if the connection has already been established and reuse the existing connection.
 const globalForPrisma = globalThis as unknown as {
-    prisma: PrismaClient | undefined
+  prisma: PrismaClient | undefined
 }
 
-export const prisma = 
-    globalForPrisma.prisma ??
-    new PrismaClient({
-        log: ['query']
-    })
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ['query'],
+  })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
