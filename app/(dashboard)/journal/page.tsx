80e1ce88e4461bf017e74280ce83b0c6e8a7cdba
ValentaCodes <valentacodes@gmail.com>
2023-09-19 20:03:22 -0600

    starting work on journal entries using auth

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -1,21 +1,23 @@
 // this will be a react server component
-// Which means we can fetch all the journal entries right here. no code will make it to the browser. 
+// Which means we can fetch all the journal entries right here. no code will make it to the browser.
 import { prisma } from '@/utils/db'
-import { User } from '@prisma/client'
-import { currentUser } from '@clerk/nextjs'
+import { getUserByClerkId } from '@/utils/auth'
+
 
 const getEntries = async () => {
-  const user = await currentUser()
-  try {
-    const data = await prisma.journalEntry.findMany({
-      where: {
-        userId: user?.id as string
-      }
-    })
-    console.log('journal_entries',data);
-  } catch (error) {
-    console.log(error)
-  }
+  const user = await getUserByClerkId()
+  console.log('user_data_from_journal', user)
+
+  //   try {
+  //     const data = await prisma.journalEntry.findMany({
+  //       where: {
+  //         userId: user as string
+  //       }
+  //     })
+  //     console.log('journal_entries',data);
+  //   } catch (error) {
+  //     console.log(error)
+  //   }
 }
 
 const JournalPage = async () => {
