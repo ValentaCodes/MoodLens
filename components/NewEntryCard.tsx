28a0cb3c55e41f09596dfbbf2e0a96256a980d8f
ValentaCodes <valentacodes@gmail.com>
2023-09-20 18:20:39 -0600

    setup new entry button function

--- components/NewEntryCard.tsx
+++ components/NewEntryCard.tsx
@@ -1,8 +1,22 @@
+'use client'
+
+import { createNewEntry } from "@/utils/api"
+import { useRouter } from "next/navigation"
+
 const NewEntryCard = () => {
+  const router = useRouter()
+  
+  const handleNewEntry = async () => {
+
+   const data = await createNewEntry()
+    router.push(`/journal/${data?.id}`)
+  }
+
   return (
-    <div className="flex flex-col border h-60 ">
-    <div className="text-1xl ">newEntryCard</div>
-    <input className="placeholder:italic" placeholder="Feel free to start typing..."></input>
+    <div className="cursor-pointer overflow-hidden rounded-lg bg-red-700 text-white shadow">
+      <div className="px-4 py-5 sm:p-6" onClick={handleNewEntry}>
+        <span className="text-2xl">New Entry</span>
+      </div>
     </div>
   )
 }
