9f6aac56499d3f069e5f537a0a3fbf283bc472c4
ValentaCodes <valentacodes@gmail.com>
2023-09-21 12:03:39 -0600

    initial styling of entry cards

--- components/EntryCard.tsx
+++ components/EntryCard.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
-const EntryCard = (entry : any) => {
+
+const EntryCard = ({ entry }) => {
+  const date: string = new Date(entry.createdAt).toDateString()
   return (
-    <div>{}</div>
+    <div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow">
+      <div className='px-4 py-5'>
+        {date}
+      </div>
+      <div className="px-4 py-5 sm:p-6">{entry?.analysis?.summary}</div>
+      <div className="px-4 py-4 sm:px-6">{entry?.analysis?.mood}</div>
+    </div>
   )
 }
 
