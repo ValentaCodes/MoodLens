4d2c11847b00be788c1b3347d0dfc0d045a8459b
ValentaCodes <valentacodes@gmail.com>
2023-09-22 20:02:07 -0600

    added type for request params

--- app/api/journal/[id]/route.ts
+++ app/api/journal/[id]/route.ts
@@ -1,18 +1,22 @@
 // This route will be used to make a patch request to update/save journal entries in the db at domain/journal/[id] of the journal entry we want to patch
 import { getUserByClerkId } from '@/utils/auth'
 import { prisma } from '@/utils/db'
-import { revalidatePath } from 'next/cache'
 import { NextResponse } from 'next/server'
+type Params = {
+  params: {
+    id: string
+  }
+}
 
-export const PATCH = async (request: Request, { params }: any) => {
-  // in next we grab the content from using the web standard "Request"
+export const PATCH = async (request: Request, {params}: Params) => {
+  // In Next we grab the the req.body by using the web standard "Request"
   const { content } = await request.json()
   const user = await getUserByClerkId()
   const updatedEntry = await prisma.journalEntry.update({
     where: {
       userId_id: {
         userId: user?.id as string,
-        id: params.id,
+        id: params?.id,
       },
     },
     data: {
