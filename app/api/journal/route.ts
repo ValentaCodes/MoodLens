77017c11d7b28eb481df08cfd922bfd08d7ad922
ValentaCodes <valentacodes@gmail.com>
2023-09-25 14:13:41 -0600

    cleanup

--- app/api/journal/route.ts
+++ app/api/journal/route.ts
@@ -16,7 +16,7 @@ export const POST = async () => {
   revalidatePath('/journal')
   return NextResponse.json({ data: entry })
 }
-// NOTE: Next.js and caching 
+// NOTE: Next.js and caching
 // Whenever a page like '/journal' gets data it's going to get cached
 // So to fix the issue where I was not seeing the new notes I created render to page unless I the refreshed page
 // I can use "revalidatePath()" to tell next.js to revalidate that cache - meaning clean it and get it again whenever there is a change on the page
