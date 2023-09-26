f6572e7399c0b57a166b5806e02a416b08ce5555
ValentaCodes <valentacodes@gmail.com>
2023-09-26 12:02:53 -0600

    added server action from props

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -2,24 +2,20 @@
 import { updateEntry } from '@/utils/api'
 import { useState } from 'react'
 import { useAutosave } from 'react-autosave'
-import { useRouter } from 'next/navigation'
 
-const Editor = ({ entry }) => {
+const Editor = ({ entry, getAnalysisData }) => {
   const [value, setValue] = useState(entry?.content)
   const [isLoading, setIsLoading] = useState(false)
-  const router = useRouter()
   useAutosave({
     data: value,
     onSave: async (_value) => {
       setIsLoading(true)
       await updateEntry(entry?.id, _value)
       setIsLoading(false)
-      setTimeout(() => {
-        router.refresh()
-      }, 300)
+      // server action that invalidates cached data
+      getAnalysisData(entry.id)
     },
   })
-
   return (
     <div className="w-full h-full">
       {isLoading && <div>...saving</div>}
