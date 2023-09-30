2cd3e1b822a58dc62a615480bf76544967c2448a
ValentaCodes <valentacodes@gmail.com>
2023-09-29 22:42:36 -0600

    added capitalization classes

--- components/EntryCard.tsx
+++ components/EntryCard.tsx
@@ -6,7 +6,7 @@ const EntryCard = ({ entry }) => {
     <div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow h-[15rem]">
       <div className="px-4 py-5 h-1/3">{date}</div>
       <div className="px-4 py-5 sm:p-6 truncate h-1/3">{entry?.analysis?.summary}</div>
-      <div className="px-4 py-5 sm:px-6 h-1/3">{entry?.analysis?.mood}</div>
+      <div className="px-4 py-5 sm:px-6 h-1/3 capitalize">{entry?.analysis?.mood}</div>
     </div>
   )
 }
