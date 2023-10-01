e18d60b09328699ea4618606a418ae4680912381
ValentaCodes <valentacodes@gmail.com>
2023-09-30 22:34:38 -0600

    added min-height to body

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+// import { Inter } from 'next/font/google'
+// const inter = Inter({ subsets: ['latin'] })
 import { ClerkProvider } from '@clerk/nextjs'
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'MoodLens',
@@ -15,9 +15,9 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+      <html lang="en">
+        <body className="min-h-screen">{children}</body>
+      </html>
     </ClerkProvider>
   )
 }
