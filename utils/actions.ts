dbefdfe9448cccd6d074eb5f55956a6e29bd0c00
ValentaCodes <valentacodes@gmail.com>
2023-10-02 20:26:36 -0600

    added type to params

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,8 +1,9 @@
 "use server"
 import { revalidatePath } from 'next/cache'
+import { Params } from './types'
 // server action
-export const revalidateAnalysisData = async (params) => {
-  revalidatePath(`/journal/${params.id}`)
+export const revalidateAnalysisData = async (params: Params) => {
+  revalidatePath(`/journal/${params?.id}`)
 }
 
 export const revalidateJournalPage = async () => {
