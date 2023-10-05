f4852a1078bd7e9071deeff3b73f3ecb0b1dc164
ValentaCodes <valentacodes@gmail.com>
2023-10-05 10:04:14 -0600

    responsive design for journal page

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -20,26 +20,26 @@ const links = [
 // Make sure to render the children. It will serve the purpose of it being a layout component
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-screen w-screen relative">
-      <aside className="absolute top-0 w-[200px] left-0 border-r h-full">
-        <div className="px-4 py-4">
-          <ul className="divide-y divide-gray-200">
-            {links.map((link) => (
-              <li className=" py-5" key={link.label}>
-                <Link href={link.href}>
-                  <p className="text-2xl">{link.label}</p>
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </aside>
-      <div className="ml-[200px] h-full">
-        <header className="h-[60px] border-b border-black/10">
-          <div className="h-full w-full px-6 flex items-center justify-end">
+    <div className="flex flex-col ">
+      <div className="sm:max-2xl:ml-[200px] sm:max-2xl:h-full">
+        <header className="sm:max-2xl:h-[60px] border-b border-black/10">
+          <div className="sm:max-2xl:h-full sm:max-2xl:w-full sm:max-2xl:px-6 flex items-center justify-end">
             <UserButton showName={true} />
           </div>
         </header>
+        <div className="sm:max-2xl:absolute sm:max-2xl:top-0 sm:max-2xl:w-[200px] sm:max-2xl:left-0 sm:max-2xl:border-r sm:max-2xl:h-full align-center">
+          <div className="sm:max-2xl:px-4 sm:max-2xl:py-4">
+            <ul className="sm:max-2xl:divide-y sm:max-2xl:divide-gray-200 flex flex-row sm:max-2xl:flex-col">
+              {links.map((link) => (
+                <li className=" py-5" key={link.label}>
+                  <Link href={link.href}>
+                    <p className="text-2xl">{link.label}</p>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
         <div className="h-[calc(100vh-60px)]">{children}</div>
       </div>
     </div>
