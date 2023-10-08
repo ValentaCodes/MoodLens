16970173710a7c68a3a235bcb7e61f64fb9c0eb3
ValentaCodes <valentacodes@gmail.com>
2023-10-07 20:25:35 -0600

    added loading state

--- components/Questions.tsx
+++ components/Questions.tsx
@@ -1,16 +1,26 @@
 'use client'
-import React, { useState } from 'react'
+import { askQuestion } from '@/utils/api'
+import React, { Suspense, useState } from 'react'
+import LoadingAnalysis from '@/app/(dashboard)/journal/[id]/loading'
 
 const QuestionForm = () => {
   const [value, setValue] = useState('')
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [response, setResponse] = useState()
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
+    setIsLoading(true)
+
+    const answer = await askQuestion(value)
+    setResponse(answer)
+    setIsLoading(false)
   }
   return (
     <div className="">
+      {isLoading ?? <LoadingAnalysis />}
       <form onSubmit={handleSubmit}>
         <input
+          disabled={isLoading}
           onChange={(e) => setValue(e.target.value)}
           value={value}
           className="rounded-lg border border-black/20 py-6 px-6 sm:max-2xl:w-3/5 sm:max-lg:text-lg"
@@ -18,12 +28,14 @@ const QuestionForm = () => {
           placeholder="Ask me a question about your journal entries..."
         />
         <button
+          disabled={isLoading}
           className="rounded-lg bg-red-700 text-white py-6 px-8 shadow"
           type="submit"
         >
           Ask
         </button>
       </form>
+      {response ?? <div>{response}</div>}
     </div>
   )
 }
