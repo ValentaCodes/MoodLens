a0f4f2bd2440a2cd318e27bd2360b8189e5f0314
ValentaCodes <valentacodes@gmail.com>
2023-10-09 23:34:15 -0600

    chart interface

--- utils/interfaces.ts
+++ utils/interfaces.ts
@@ -1,19 +1,32 @@
-export default interface Entry {
-        id: string
-        createdAt: Date
-        updatedAt: Date
-        userId: string
-        content: string
-        analysis: {
-            id: string
-            createdAt: Date
-            updatedAt: Date
-            entryId: string
-            mood: string
-            summary: string
-            color: string
-            negative: boolean
-            subject: string
-            sentimentScore: number
-        } | null
-} 
+export interface Entry {
+  id: string
+  createdAt: Date
+  updatedAt: Date
+  userId: string
+  content: string
+  analysis: {
+    id: string
+    createdAt: Date
+    updatedAt: Date
+    entryId: string
+    mood: string
+    summary: string
+    color: string
+    negative: boolean
+    subject: string
+    sentimentScore: number
+  } | null
+}
+export interface Analysis {
+  id: string
+  createdAt: Date
+  updatedAt: Date
+  entryId: string
+  mood: string
+  summary: string
+  color: string
+  negative: boolean
+  subject: string
+  sentimentScore: number
+  avg: number
+}
