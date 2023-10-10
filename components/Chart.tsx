f9cd2dccbe0c43b45c0b218edbdda4a61fbde665
ValentaCodes <valentacodes@gmail.com>
2023-10-10 17:07:34 -0600

    starting chart work

--- components/Chart.tsx
+++ components/Chart.tsx
@@ -1,14 +1,26 @@
 'use client'
 
 import React from 'react'
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts'
 
-const Chart = ({ analysis }: any) => {
-  // const date = analysis?.createdAt.getDate()
-  console.log(analysis?.sentimentScore)
+const Chart = ({ data }: any) => {
+  console.log(data);
+  
   return (
     <div>
-      Score: {}
-      <div>Average: {}</div>
+      <LineChart
+        width={600}
+        height={300}
+        data={data.analysis}
+        margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
+      >
+        <Line type="monotone" dataKey="sentimentScore" stroke="#8884d8" />
+        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+        <XAxis dataKey="createdAt" />
+        <YAxis dataKey="sentimentScore" />
+        <Tooltip />
+      </LineChart>
+      <div>Average: {data.avg}</div>
     </div>
   )
 }
