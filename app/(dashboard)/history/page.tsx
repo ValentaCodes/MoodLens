ff4a004208ef1951acdd074c034b68da72a231a3
ValentaCodes <valentacodes@gmail.com>
2023-10-09 23:33:36 -0600

    started history page

---
+++ app/(dashboard)/history/page.tsx
@@ -0,0 +1,36 @@
+import Chart from '@/components/Chart'
+import { getUserByClerkId } from '@/utils/auth'
+import { prisma } from '@/utils/db'
+import React from 'react'
+
+
+const getData = async () => {
+  const user = await getUserByClerkId()
+  const analysis = await prisma.analysis.findMany({
+    where: {
+      userId: user?.id,
+    },
+    select: {
+      sentimentScore: true,
+      createdAt: true,
+    },
+  })
+  const sum = analysis.reduce((previousValue, currentValue) => {
+    return previousValue + currentValue.sentimentScore
+  }, 0)
+  const avg = Math.round(sum / analysis.length)
+
+  return { analysis, avg }
+}
+
+const HistoryPage = async () => {
+  const analysis = await getData()
+  return (
+    <div>
+      <Chart analysis={analysis}></Chart>
+      {/* <div>Average {score.avg}</div> */}
+    </div>
+  )
+}
+
+export default HistoryPage
