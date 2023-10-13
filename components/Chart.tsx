45d3a28e77fe65f6b09d72565facad77935fff74
ValentaCodes <valentacodes@gmail.com>
2023-10-13 10:30:14 -0600

    Formatted date, styled values

--- components/Chart.tsx
+++ components/Chart.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react'
 import {
+  Label,
   LineChart,
   Line,
   CartesianGrid,
@@ -11,19 +12,32 @@ import {
 } from 'recharts'
 
 const Chart = ({ data }: any) => {
+  // destructure data for formatting
+  const { analysis, date, avg } = data
+
+  // NOTE: may be a better way to do the following
+  analysis.map((item: any) => {
+    date.forEach((date: any) => {
+      return (item.createdAt = date.createdAt)
+    })
+  })
   return (
-      <LineChart
-        width={730}
-        height={300}
-        data={data.analysis}
-        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-      >
-        <CartesianGrid stroke="#ccc" strokeDasharray={5} />
-        <Line type="monotone" dataKey="sentimentScore" stroke="#8884d8" />
-        <XAxis dataKey="createdAt" order={'dsc'}/>
-        <YAxis />
-        <Tooltip />
-      </LineChart>
+    <LineChart
+      width={730}
+      height={300}
+      data={analysis}
+      margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+    >
+      <CartesianGrid stroke="#ccc" strokeDasharray={5} />
+      <XAxis dataKey="createdAt">
+        <Label value="Date" offset={0} position={'insideBottom'} />
+      </XAxis>
+      <Line type="monotone" dataKey="sentimentScore" stroke="#8884d8" />
+      <YAxis
+        label={{ value: 'Sentiment Score', angle: -90, position: 'insideLeft' }}
+      />
+      <Tooltip />
+    </LineChart>
   )
 }
 
