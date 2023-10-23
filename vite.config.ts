0caada393068aa9d4d504917002353513c87363d
ValentaCodes <valentacodes@gmail.com>
2023-10-23 13:34:09 -0600

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
+// / <reference types="vitest"/>
+
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-    plugins: [react()],
-    test: {
-        include: ['**/__test__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
-        globals: true, 
-        environment: 'jsdom',
-        setupFiles: 'setupTests',
-        mockReset: true,
-    },
+  plugins: [react()],
+  test: {
+    include: ['**/__test__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: 'setupTests',
+    mockReset: true,
+  },
 })
