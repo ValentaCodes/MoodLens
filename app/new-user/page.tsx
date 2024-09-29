1a6d0ea33b3f757c5a4e089c7af0f4152c9bf4b1
ValentaCodes <valentacodes@gmail.com>
2024-09-29 06:42:06 -0600

    fixed user sign-up/sign-in auth

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,25 +1,30 @@
-import { prisma } from '@/utils/db'
-import { currentUser } from '@clerk/nextjs'
+
+import {prisma} from '@/utils/db'
 import { redirect } from 'next/navigation'
-import type { User } from '@clerk/nextjs/api'
+import type { User } from '@clerk/backend'
+import { useUser } from '@clerk/clerk-react'
+import { currentUser } from '@clerk/nextjs/server'
+import { Prisma } from '@prisma/client'
 
 const createNewUser = async () => {
-    const user: User | null = await currentUser()
 
-    const findUser = await prisma.user.findUnique({
-      where: {
+  const user: User | null = await currentUser()
+
+  const findUser = await prisma.user.findUnique({
+    where: {
+      clerkId: user?.id as string,
+    },
+  })
+
+
+  if (!findUser) {
+    await prisma.user.create({
+      data: {
         clerkId: user?.id as string,
+        email: user?.emailAddresses[0].emailAddress as string,
       },
     })
-
-    if (!findUser) {
-      await prisma.user.create({
-        data: {
-          clerkId: user?.id as string,
-          email: user?.emailAddresses[0].emailAddress as string,
-        },
-      })
-    }
+  }
   redirect('/journal')
 }
 
--- utils/db.ts
+++ utils/db.ts
@@ -1,15 +1,29 @@
+// import { PrismaClient } from '@prisma/client'
+// // This is how we will make a new instance for prisma client
+// // With hot module reloading, multiple database connection objects can be created
+// // Causing issues in the application. These utility methods check if the connection has already been established and reuse the existing connection.
+// const globalForPrisma = globalThis as unknown as {
+//   prisma: PrismaClient 
+// }
+
+// export const prisma =
+//   globalForPrisma.prisma ??
+//   new PrismaClient({
+//     log: ['query'],
+//   })
+
+// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+import { Pool, neonConfig } from '@neondatabase/serverless'
+import { PrismaNeon } from '@prisma/adapter-neon'
 import { PrismaClient } from '@prisma/client'
-// This is how we will make a new instance for prisma client
-// With hot module reloading, multiple database connection objects can be created
-// Causing issues in the application. These utility methods check if the connection has already been established and reuse the existing connection.
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient 
-}
+import dotenv from 'dotenv'
+import ws from 'ws'
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ['query'],
-  })
+dotenv.config()
+neonConfig.webSocketConstructor = ws
+const connectionString = `${process.env.DATABASE_URL}`
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+const pool = new Pool({ connectionString })
+const adapter = new PrismaNeon(pool)
+export const prisma = new PrismaClient({ adapter })
